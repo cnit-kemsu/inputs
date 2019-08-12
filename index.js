f5ce98fb3de422141ea91ecb0c3eb2e059d58c18
John Cooper <johnxcooper87@gmail.com>
2019-08-12 11:55:48 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ export { default as TextField }  from './src/inputs/TextField';
 export { default as Checkbox }  from './src/inputs/Checkbox';
 export { default as Select }  from './src/inputs/Select';
 export { default as DateTimePicker }  from './src/inputs/DateTimePicker';
+export { default as Editor }  from './src/inputs/Editor';
 export { default as PickersUtilsProvider }  from './src/PickersUtilsProvider';
 export { deserializeDate } from './src/lib/deserializeDate';
 export { deserializeEditorContent } from './src/lib/deserializeEditorContent';
