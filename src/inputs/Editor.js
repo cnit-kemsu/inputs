3ea5bcd0bfb2aa02db1f50dd865bb5a49bed74aa
John Cooper <johnxcooper87@gmail.com>
2019-08-19 09:44:49 +0700

    update

--- src/inputs/Editor.js
+++ src/inputs/Editor.js
@@ -1,6 +1,6 @@
 import React, { createElement } from 'react';
 import { useField } from '@kemsu/form';
-import { Editor as DraftEditor, convertStateToRawContent, createEditorStateWithContent, blobs } from '@kemsu/editor';
+import { Editor as DraftEditor, convertEditorStateToRawContent, createEditorStateFromContent, blobs } from '@kemsu/editor';
 
 function toFiles(blobUrl) {
   return blobs[blobUrl];
@@ -8,7 +8,7 @@ function toFiles(blobUrl) {
 
 const EditorProps = {
   serialize(value) {
-    const rawContent = convertStateToRawContent(value);// |> JSON.stringify |> JSON.parse;
+    const rawContent = convertEditorStateToRawContent(value);// |> JSON.stringify |> JSON.parse;
     const _blobs = [];
     for (const entity of Object.values(rawContent.entityMap)) {
       if (entity.type === 'IMAGE' && entity.data.src.substring(0, 4) === 'blob') {
@@ -23,7 +23,7 @@ const EditorProps = {
     }
     return [
       rawContent,
-      _blobs.map(toFiles)
+      _blobs.length > 0 ? _blobs.map(toFiles) : undefined
     ];
   }
 };
@@ -31,9 +31,9 @@ const EditorProps = {
 function Editor({ comp, name, validate }) {
 
   const { value, error, touched, dirty, onChange, onBlur } = useField(comp, name, validate, EditorProps);
-  //console.log(value ? convertStateToRawContent(value) : undefined);
+  //console.log(value ? convertEditorStateToRawContent(value) : undefined);
   return createElement(DraftEditor, {
-    editorState: value || createEditorStateWithContent(),
+    editorState: value || createEditorStateFromContent(),
     onChange: onChange,
     onBlur: onBlur
   });
--- src/lib/deserializeEditorContent.js
+++ src/lib/deserializeEditorContent.js
@@ -1,7 +1,7 @@
-import { createEditorStateWithContent } from '@kemsu/editor';
+import { createEditorStateFromContent } from '@kemsu/editor';
 
 export function deserializeEditorContent(value) {
-  if (!value) return createEditorStateWithContent();
-  if (value instanceof Object && value?.constructor?.name !== 'EditorState') return createEditorStateWithContent(value);
+  if (!value) return createEditorStateFromContent();
+  if (value instanceof Object && value?.constructor?.name !== 'EditorState') return createEditorStateFromContent(value);
   return value;
 }
